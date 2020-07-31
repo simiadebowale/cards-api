c858bb7871703fd38e8ab01923afc1d2faeccb92
Simi Adebowale <adebowalesimileoluwa@gmail.com>
2020-07-31 19:36:07 -0400

    Updates

---
+++ precache-manifest.a150bb4d1595594229448b59565d87cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a9b822af216f691ce4e564ec0aeff2",
+    "url": "/cards-api/index.html"
+  },
+  {
+    "revision": "2ad5bee50417165535e5",
+    "url": "/cards-api/static/css/main.23b807c8.chunk.css"
+  },
+  {
+    "revision": "da5cc93eb36b8572344d",
+    "url": "/cards-api/static/js/2.aa5c5127.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cards-api/static/js/2.aa5c5127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad5bee50417165535e5",
+    "url": "/cards-api/static/js/main.33f1a6c0.chunk.js"
+  },
+  {
+    "revision": "701779716cbec5dd2030",
+    "url": "/cards-api/static/js/runtime-main.53606d5c.js"
+  }
+]);
